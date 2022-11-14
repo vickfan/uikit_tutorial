780137dd83b35589d6f7e4e1fda05e0842e12d8c
[Vick Fan] <[vickfct@gmail.com]>
2022-11-14 20:22:01 +0800

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Chi To Fan on 14/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
